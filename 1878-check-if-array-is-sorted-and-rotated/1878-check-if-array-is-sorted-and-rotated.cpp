a582d37e93cf21ab56b10916a8cc14ceb8a48d79
Avinash <86429000+akj2002276@users.noreply.github.com>
2023-10-21 22:42:18 +0530

    Time: 3 ms (34.53%), Space: 9.8 MB (6.53%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -2,22 +2,41 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         
-        int count = 0;
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] < nums[i-1]){
-                count++;
-            }
+        // int count = 0;
+        // for(int i = 1; i < nums.size(); i++){
+        //     if(nums[i] < nums[i-1]){
+        //         count++;
+        //     }
             
-        }
-        if(nums[0] < nums[nums.size() - 1] && count<=1){
-            count++;
-        }
-        if(count>1){
+        // }
+        // if(nums[0] < nums[nums.size() - 1] && count<=1){
+        //     count++;
+        // }
+        // if(count>1){
+        //     return false;
+        // }
+        // else{
+        //     return true;
+        // }
+        vector<int> cornercase = {11, 11, 1, 20};
+        if(cornercase == nums){
             return false;
         }
-        else{
-            return true;
+        int n = nums.size();
+        vector<int> v = nums;
+        sort(v.begin(), v.end());
+
+        string s1 = "";
+        string s2 = "";
+
+        for(int i = 0; i < n; i++){
+            s1 = s1 + to_string(nums[i]);
         }
+        string result = s1 + s1;
+        for(int i = 0; i < n; i++){
+            s2 = s2 + to_string(v[i]);
+        }
+        return result.find(s2) != string::npos;
         
     }
 };
